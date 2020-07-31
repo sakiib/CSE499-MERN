cad93e098dc391e40f3b8d93ddd442def5b7b570
Sakib Alamin <sakibalamin162@gmail.com>
2020-08-01 00:27:32 +0600

    commit after second vid

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./database/db');
+
+connectDB();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`listening on port ${port}`));
