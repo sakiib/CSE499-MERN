e5f2bb8bf48db2ef66eb9afcdf80dd446af594e0
Sakib Alamin <sakibalamin162@gmail.com>
2020-08-01 14:55:48 +0600

    commit after 11th vid

--- server.js
+++ server.js
@@ -2,19 +2,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
-
+const authRoutes = require('./routes/auth');
 
 // Middleware
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+app.use('/api/auth', authRoutes);
+
 
 const connectDB = require('./database/db');
 connectDB();
-
-app.get('/', (req, res) => {
-    res.send('Inside Server!');
-});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening on port ${port}`));
