b68f78aae30e01da919cbc36e8c635ba48f5ba56
Sakib Alamin <sakibalamin162@gmail.com>
2020-08-02 21:07:12 +0600

    commit after 18th vid

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { jwtExpire, jwtSecret } = require('../config/keys');
 
 exports.signupController = async (req, res) => {
     const { username, email, password } = req.body;
@@ -28,5 +30,40 @@ exports.signupController = async (req, res) => {
 };
 
 exports.signinController = async (req, res) => {
-    
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                errorMessage: 'Invalid credentials',
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                errorMessage: 'Invalid credentials',
+            });
+        }
+        
+        const payload = {
+            user: {
+                _id: user._id,
+            },
+        };
+        jwt.sign(payload, jwtSecret, { expiresIn: jwtExpire }, (err, token) => {
+            if (err) console.log('jwt error: ', err);
+            const { _id, username, email, role } = user;
+            res.json({
+                token,
+                user: { _id, username, email, role },
+            });
+        });
+    } catch (err) {
+        console.log('signinController error: ', err);
+        res.status(500).json({
+            errorMessage: 'Server error',
+        });
+    }
 };
