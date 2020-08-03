eda98c05dedf5b326f839f9187e79fc39a4557cb
Sakib Alamin <sakibalamin162@gmail.com>
2020-08-03 13:54:21 +0600

    commit after 28th vid

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
 const AdminDashboard = () => {
+    const showHeader = () => (
+        <div className='bg-dark text-white py-4'>
+            <div className='container'> 
+                <div className='row'>
+                    <div className='col-md-6'> 
+                        <h1> 
+                            <i className='fas fa-home'> Dashboard </i>
+                        </h1>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    )
     return (
-        <div> Inside AdminDashboard </div>
+        <section>  
+            { showHeader() }
+        </section>
     )
 }
 
